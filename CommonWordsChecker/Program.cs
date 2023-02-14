b94b6d8c6294887878f70737c9b772d8d581c432
Aggelos <aggelos.gkampis@gmail.com>
2023-02-14 11:34:38 +0200

    Updated version that works for excel sheets

--- CommonWordsChecker/Program.cs
+++ CommonWordsChecker/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using OfficeOpenXml;
 
 namespace ExcelCommonWords
 {
@@ -7,33 +9,35 @@ namespace ExcelCommonWords
     {
         static void Main(string[] args)
         {
-            string[] column1 = new string[] { "" };
-            string[] column2 = new string[] { "δσα omega", "α " };
-            List<string> commonWords = new List<string>();
+            var commonWords = new List<string>();
+            var commonWordRowsInColumn1 = new List<string>();
+            var commonWordRowsInColumn2 = new List<string>();
+            var file = new ExcelPackage(new System.IO.FileInfo(@"C:\Users\Isocratis-4\source\repos\CommonWordsChecker\CommonWordsChecker\test1.xlsx"));
+            var worksheet = file.Workbook.Worksheets[0];
 
-            for (int i = 0; i < column1.Length; i++)
+            var column1 = string.Join(" ", worksheet.Cells["A:A"].Select(c => c.Value?.ToString().Trim().ToLower()));
+            var column2 = string.Join(" ", worksheet.Cells["B:B"].Select(c => c.Value?.ToString().Trim().ToLower()));
+
+            var words1 = column1.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var words2 = column2.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var word1 in words1)
             {
-                string[] words1 = column1[i].Split(' ');
-                foreach (string word1 in words1)
+                foreach (var word2 in words2)
                 {
-                    for (int j = 0; j < column2.Length; j++)
+                    if (string.Equals(word1, word2, StringComparison.OrdinalIgnoreCase)
+                        && !commonWords.Contains(word1))
                     {
-                        string[] words2 = column2[j].Split(' ');
-                        foreach (string word2 in words2)
-                        {
-                            if (word1.ToLower() == word2.ToLower() && !commonWords.Contains(word1.ToLower()))
-                            {
-                                commonWords.Add(word1.ToLower());
-                                Console.WriteLine($"Common word found: {word1}");
-                            }
-                        }
+                        commonWords.Add(word1);
+                        commonWordRowsInColumn1.Add($"Row {Array.IndexOf(words1, word1) + 1}");
+                        commonWordRowsInColumn2.Add($"Row {Array.IndexOf(words2, word2) + 1}");
+                        Console.WriteLine($"Common word found: {word1} in Row {Array.IndexOf(words1, word1) + 1} of Column 1 and Row {Array.IndexOf(words2, word2) + 1} of Column 2");
                     }
                 }
             }
 
-        }
 
+            file.Dispose();
+        }
     }
 }
-
-
