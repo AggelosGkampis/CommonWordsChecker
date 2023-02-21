162d3ff746428058ec843988d9884832d6576e26
Aggelos <aggelos.gkampis@gmail.com>
2023-02-21 16:20:43 +0200

    Ok

--- DesktopAppCommonWords/Program.cs
+++ DesktopAppCommonWords/Program.cs
@@ -2,7 +2,9 @@ using OfficeOpenXml;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Windows.Forms;
+using static OfficeOpenXml.ExcelErrorValue;
 
 namespace DesktopAppCommonWords
 {
@@ -22,16 +24,26 @@ namespace DesktopAppCommonWords
 
         public static DataTable GetCommonWords(ExcelWorkbook workbook)
         {
-            var commonWords = new HashSet<string>();
+            //var commonWords = new HashSet<string>();
             var worksheet = workbook.Worksheets[0];
             var ignoredWords = new HashSet<string> { "THE", "I", "A", "YOU", "HE", "SHE", "IT", "WE", "THEY", "AN", "AND", "OR",
                                      "BUT", "SO", "IN", "ON", "AT", "IS", "ARE", "WAS", "WERE", "BE", "BEEN", "AM", "DO", "DOES", "DID", "HAVE",
                                      "HAS", "HAD", "OF", "TO", "FOR", "FROM", "BY", "WITH", "THAT", "THIS", "THOSE", "THESE", "IF", "THEN", "ELSE",
-                                     "WHEN", "WHERE", "WHILE", "HOW", "WHAT", "WHO", "WHOM", "WHOSE", "NOT", "NO", "YES", "TRUE", "FALSE", "NULL" };
+                                     "WHEN", "WHERE", "WHILE", "HOW", "WHAT", "WHO", "WHOM", "WHOSE", "NOT", "NO", "YES", "TRUE", "FALSE", "NULL", "foreign" };
 
             var column1Words = new Dictionary<string, List<string>>();
             var column2Words = new Dictionary<string, List<string>>();
 
+            column1Words = DictionariesWithSplitWords(ignoredWords, column1Words, worksheet);
+            column2Words = DictionariesWithSplitWords2(ignoredWords, column2Words, worksheet);
+            DataTable table = CreateDatatableOutput(column1Words, column2Words, worksheet);
+
+            return table;
+        }
+
+
+        public static Dictionary<string, List<string>> DictionariesWithSplitWords(HashSet<string> ignoredWords, Dictionary<string, List<string>> column1Words, ExcelWorksheet worksheet)
+        {
             // Split words in column 1 and track the row numbers where each word occurs
             for (int i = 1; i <= worksheet.Dimension.Rows; i++)
             {
@@ -44,8 +56,13 @@ namespace DesktopAppCommonWords
                                  .ToList();
 
                 column1Words[cellAddress] = words;
-            }
 
+
+            }
+            return column1Words;
+        }
+        public static Dictionary<string, List<string>> DictionariesWithSplitWords2(HashSet<string> ignoredWords, Dictionary<string, List<string>> column2Words, ExcelWorksheet worksheet)
+        {
             // Split words in column 2 and track the row numbers where each word occurs
             for (int i = 1; i <= worksheet.Dimension.Rows; i++)
             {
@@ -57,9 +74,16 @@ namespace DesktopAppCommonWords
                                  .Where(w => !ignoredWords.Contains(w, StringComparer.OrdinalIgnoreCase))
                                  .ToList();
 
-                column2Words[cellAddress] = words;
-            }
 
+                column2Words[cellAddress] = words;
+
+
+            }
+            return column2Words;
+        }
+
+        public static DataTable CreateDatatableOutput(Dictionary<string, List<string>> column1Words, Dictionary<string, List<string>> column2Words, ExcelWorksheet worksheet)
+        {
             DataTable result = new DataTable();
             result.Columns.Add("Common Word");
             result.Columns.Add("Column 1 Matches");
@@ -73,21 +97,51 @@ namespace DesktopAppCommonWords
 
                 foreach (var column1Match in column1Matches)
                 {
-                    foreach (var column2Match in column2Matches)
+                    var column1Value = worksheet.Cells[column1Match].Value.ToString();
+
+                    // check if the column1 value is already present in the result
+                    var existingRow = result.AsEnumerable().FirstOrDefault(r =>
+                        string.Equals(r.Field<string>("Column 1 Matches"), column1Value, StringComparison.OrdinalIgnoreCase) &&
+                        string.Equals(r.Field<string>("Common Word"), word, StringComparison.OrdinalIgnoreCase));
+
+                    if (existingRow == null)
                     {
+                        // add a new row to the result table
                         var row = result.NewRow();
                         row["Common Word"] = word;
-                        row["Column 1 Matches"] = worksheet.Cells[column1Match].Value;
-                        row["Column 2 Matches"] = worksheet.Cells[column2Match].Value;
+                        row["Column 1 Matches"] = column1Value;
                         result.Rows.Add(row);
+                    }
+
+                    // add column2 matches to the existing row or a new row
+                    foreach (var column2Match in column2Matches)
+                    {
+                        var column2Value = worksheet.Cells[column2Match].Value.ToString();
+
+                        existingRow = result.AsEnumerable().FirstOrDefault(r =>
+
+                            string.Equals(r.Field<string>("Column 2 Matches"), column2Value, StringComparison.OrdinalIgnoreCase) &&
+                                                string.Equals(r.Field<string>("Common Word"), word, StringComparison.OrdinalIgnoreCase));
+
+                        if (existingRow == null)
+                        {
+                            var row = result.NewRow();
+                            row["Common Word"] = word;
+                            row["Column 2 Matches"] = column2Value;
+                            result.Rows.Add(row);
+                        }
+                        else if (existingRow["Column 1 Matches"].ToString() != column1Value)
+                        {
+                            // add column1 match to the existing row if it's not already present
+                            existingRow["Column 1 Matches"] += $",{column1Value}";
+                        }
                     }
                 }
             }
 
             return result;
         }
-
-
-
     }
+
 }
+
